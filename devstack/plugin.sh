f7c2b983860ac6a3763797b7c7cfd5482bd50bc2
vsaienko <vsaienko@mirantis.com>
2015-12-11 13:14:00 +0200

    Add configure_provision_network function
    This change allows to configure ironic provision network by setting
    IRONIC_PROVISION_NETWORK_NAME variable. In this case additional
    interface $OVS_PHYSICAL_BRIDGE.$IRONIC_PROVISION_SEGMENTATION_ID
    will be configured with IRONIC_PROVISION_SUBNET_GATEWAY ip address.
    
    Additional configuration parameters are:
    
    IRONIC_PROVISION_PROVIDER_NETWORK_TYPE
    IRONIC_PROVISION_SEGMENTATION_ID
    IRONIC_PROVISION_ALLOCATION_POOL
    IRONIC_PROVISION_SUBNET_GATEWAY
    IRONIC_PROVISION_SUBNET_PREFIX
    
    Change-Id: I619f2fb92aafe7348b0a47eaaaad1790df5ae5c7
    Partial-bug: #1526403

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -21,6 +21,14 @@ if is_service_enabled ir-api ir-cond; then
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # Initialize ironic
         init_ironic
+        if ! is_ironic_hardware; then
+            echo_summary "Creating bridge and VMs"
+            create_bridge_and_vms
+        fi
+        if [[ -n "${IRONIC_PROVISION_NETWORK_NAME}" ]]; then
+            echo_summary "Configuring Ironic provisioning network"
+            configure_ironic_provision_network
+        fi
 
         # Start the ironic API and ironic taskmgr components
         echo_summary "Starting Ironic"
@@ -30,6 +38,7 @@ if is_service_enabled ir-api ir-cond; then
 
     if [[ "$1" == "unstack" ]]; then
         stop_ironic
+        cleanup_ironic_provision_network
         cleanup_baremetal_basic_ops
     fi
 
